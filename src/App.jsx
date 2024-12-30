0c6fbdbfbbfe57b047055af110aa0cf06a2aa269
manish2588 <manishkc258@gmail.com>
2024-12-30 16:08:53 +0545

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AppLayout from "./components/AppLayout";
 import "./App.css";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   const router=createBrowserRouter([
     {
       path:"/",
-      element:<AppLayout/>
+      element:<AppLayout/>,
+      children:[
+        {
+          path:"about",
+          element:<AboutPage/>
+        }
+      ]
     }
   ])
   return (
