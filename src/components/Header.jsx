f12bc9b2182c4b0af97a913fb8a90fb7bb05ef46
manish2588 <manishkc258@gmail.com>
2025-02-16 16:19:20 +0545

    Added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,8 +9,8 @@ function Header() {
       initial={{opacity:0}}
       animate={{opacity:[0.5,1]}}
       transition={{
-        duration:1,
-        ease:easeInOut
+        duration:0.5,
+       
       }}
       >
         
@@ -25,15 +25,15 @@ function Header() {
         <div className="flex flex-col justify-center items-center h-full w-full relative z-10">
           <motion.h1 className="text-white font-serif text-6xl font-medium text-center"
           initial={{
-            x:-500
+            y:40
           }}
           animate={{
-            x:0,
+            y:0,
             transition:{
               duration:0.5,
               type:"spring",
               damping:30,
-              ease:easeInOut
+              
               
             }
           }}
@@ -42,16 +42,16 @@ function Header() {
           </motion.h1>
           <motion.h1 className="text-white font-serif text-6xl font-medium text-center"
            initial={{
-            x:1000
+            y:40
           }}
           animate={{
-            x:0,
+            y:0,
             transition:{
               duration:0.5,
               type:"spring",
               damping:30,
-              ease:easeInOut,
-              delay:0.5
+              
+              
               
             }
           }}
